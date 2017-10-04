db56774f688f1001ad2afbf51530db0df1d04e5e
senaingrid <ingrid@back4app.com>
2017-10-04 20:21:22 -0300

    creating pet

--- scripts/main.js
+++ scripts/main.js
@@ -1,153 +1,91 @@
-Parse.initialize("My_App_ID");
+Parse.initialize("PhmFwoYApJIR6Kz8fO1jxtlUXEmNxz9CmwQlpYma"); //PASTE YOUR Back4App APPLICATION ID
 Parse.serverURL = "https://parseapi.back4app.com/";
-Parse.javaScriptKey = "My_JS_ID";
+Parse.javaScriptKey = "oBxOBGpgHLNCmXHbLA3xo5DLKNBMWshmGOXe25ku"; //PASTE YOUR Javascript KEY
+
+// EXPLICAR PARA NAO CRIAR CLASSE USER PORQUE ELA JÁ EXISTE = Parse.User()
 
 
-//Create /save
-var Pessoa = Parse.Object.extend("Pessoa");
-var pessoa1 = new Pessoa();
+var mypet, textName, textAge, textMessage, findpet, query;
 
-pessoa1.set("idade", 27);
-pessoa1.set("nome", "Ingrid Um");
-pessoa1.set("desenvolvedor", true);
+var Pet = Parse.Object.extend("Pet");
 
-pessoa1.save(null, {
-  success: function(response) {
-    alert('Novo objeto criado com objectId: ' + response.id);
-  },
-  error: function(response, error) {
-    alert('Erro: ' + error.message);
-  }
-});
+//buttons
+var savePet = document.getElementById("save");
+var createPet = document.getElementById("create");
+var readPet = document.getElementById("read");
+var updatePet = document.getElementById("update");
+var deletePet = document.getElementById("delete");
 
-//Criar um objeto já salvando
-var Pessoa2 = Parse.Object.extend("Pessoa");
-var pessoa2 = new Pessoa();
+createPet.onclick = function () {
+  //abre form input com botão submit
+  getElements();
+  newPet();
+}
 
-pessoa2.save({
-  idade: 37,
-  nome: "Ingrid Dois",
-  desenvolvedor: false
-}, {
-  success: function(response) {
-    alert('Novo objeto criado com objectId: ' + response.id);
-  },
-  error: function(response, error) {
-    alert('Erro: ' + error.message);
-  }
+readPet.onclick = function () {
+  //abre form input com botão submit
+  getElements();
+  //retrieve - using find first
 
-});
+}
 
-//Apagando Objeto
-consultado.destroy({
-  success: function(response) {
-  	console.log('Objeto apagado');
-  },
-  error: function(response, error) {
-  	console.log('ERRO: ' + error.message);
-  }
-});
+updatePet.onclick = function () {
+  //abre form input com botão submit
+  //considerando que ja fiz o retrieve acima:
+  //check retrieve
+  //set (update)
+
+}
+
+deletePet.onclick = function () {
+  //abre form input com botão submit
+  //considerando que ja fiz o retrieve acima:
+  //check retrieve
+  //delete
 
 
+}
 
-//Update
-pessoa2.set('idade', 38);
+function getElements () {
+  textName = document.getElementById("input_name").value;
+  textAge = document.getElementById("input_age").value;
+  textMessage = document.getElementById("message");
+}
 
-pessoa2.save(null, {
-  success: function(response) {
-    alert('Objeto salvo com objectId: ' + response.id);
-  },
-  error: function(response, error) {
-    alert('Erro: ' + error.message);
-  }
-});
+//Creating/saving
+function newPet () {
+  mypet = new Pet(); //TODO: posso deixar local? ai eu não preciso criar uma variavel para cada newuser, eu reuso essa certo?
 
-//Retrieve em um objeto
-var Pessoa = Parse.Object.extend("Pessoa");
-var consultado = new Pessoa();
-var query = new Parse.Query(Pessoa);
-query.get("PASTE_HERE_OBJECT_ID", {
-  success: function(response) {
-    alert('Objeto trazido com objectId: ' + response.id);
-	   consultado = response;
-  },
-  error: function(object, error) {
-    alert('Erro: ' + error.message);
-  }
-});
+  mypet.set("name", textName);
+  mypet.set("age", textAge);
 
-//Uso do Get
-var idade = consultado.get("idade");
-var nome = consultado.get("nome");
-var desenvolvedor = consultado.get("desenvolvedor");
+  mypet.save(null, {
+    success: function(mypet) {
+      textMessage.innerHTML = 'Pet created with objectId: ' + mypet.id + ', Name: ' + mypet.get("name") + ', Age: ' + mypet.get("age");
+      alert('Pet create with success!');
+    },
+    error: function(response, error) {
+      textMessage.innerHTML = error.message;
+      alert('Erro: ' + error.message);
+    }
+  });
+}
 
-console.log(idade);
-console.log(nome);
-console.log(desenvolvedor);
+//Retrieve
+function retrievePet (){
+  findpet = new Pessoa(); //TODO: posso deixar local? ai eu não preciso criar uma variavel para cada newuser, eu reuso essa certo?
+  query = new Parse.Query(Pessoa);
 
-//Reserved
-var objectId = consultado.id;
-var updatedAt = consultado.updatedAt;
-var createdAt = consultado.createdAt;
+  //como pegar objectId pelo name
 
-console.log(objectId);
-console.log(updatedAt);
-console.log(createdAt);
+  query.get("PASTE_HERE_OBJECT_ID", {
+    success: function(response) {
+      alert('Objeto trazido com objectId: ' + response.id);
+       consultado = response;
+    },
+    error: function(object, error) {
+      alert('Erro: ' + error.message);
+    }
+  });
 
-// If you need to refresh an object you already have with the latest data that is
-// in the Parse Cloud, you can call the fetch method like so:
-consultado.fetch({
-  success: function(response) {
-        // The object was refreshed successfully.
-    console.log("Sucesso!");
-  },
-  error: function(response, error) {
-    // The object was not refreshed successfully.
-// error is a Parse.Error with an error code and message.
-    console.log("Erro! " + error.message);
-  }
-});
-
-var idade = consultado.get("idade");
-var nome = consultado.get("nome");
-var desenvolvedor = consultado.get("desenvolvedor");
-
-console.log(idade);
-console.log(nome);
-console.log(desenvolvedor);
-
-//Update
-consultado.set("idade", 27);
-consultado.save();
-
-//Arrays
-consultado.addUnique("skills", "Python");
-consultado.addUnique("skills", "Java");
-consultado.save(null, {
-  success: function(response) {
-    alert('Objeto salvo com objectId: ' + response.id);
-  },
-  error: function(response, error) {
-    alert('Erro: ' + error.message);
-  }
-});
-
-
-
-//Consultando
-carro1.fetch({
-  success: function(response) {
-    console.log("Sucesso!");
-  },
-  error: function(response, error) {
-    console.log("Erro! " + error.message);
-  }
-});
-
-var pessoa4 = carro1.get("parent");
-pessoa4.fetch({
-  success: function(post) {
-    var nome = post.get("nome");
-    console.log(nome);
-  }
-});
+}
